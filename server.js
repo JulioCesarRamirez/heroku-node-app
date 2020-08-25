1141799c1bb57ca69be1da091c5443bc03b7d062
Julio Cesar Ramirez Jimenez <julio_marshal@hotmail.com>
2020-08-24 19:41:28 -0500

    script to strat

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const hbs = require("hbs");
-require("./hbs/helpers")
+require("./hbs/helpers");
+
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 // Express hsb engine
 hbs.registerPartials(__dirname + "/views/partials", function (err) {});
 app.set("view engine", "hbs");
-
-
 
 app.get("/", function (req, res) {
   res.render("home", {
@@ -19,6 +19,6 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, () => {
-  console.log("Escuchando peticiones en el puerto 3000");
+app.listen(port, () => {
+  console.log(`Escuchando peticiones en el puerto ${port}`);
 });
